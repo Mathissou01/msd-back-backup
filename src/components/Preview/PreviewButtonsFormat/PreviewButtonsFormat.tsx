db4df81c6c49c63a1774d867a16783411247f39f
Hasan Awwad <hasan.awwad@smile.fr>
2023-04-04 15:32:38 +0000

    Merged PR 1665: feat: #2393 Preview for desktop with css
    - feat: #2396 preview mobile view
    - feat: #2397 preview tablet view
    - feat: #2393 Preview for desktop with css
    
    Related work items: #2393, #2396, #2397

--- src/components/Preview/PreviewButtonsFormat/PreviewButtonsFormat.tsx
+++ src/components/Preview/PreviewButtonsFormat/PreviewButtonsFormat.tsx
@@ -1,8 +1,10 @@
-import "./preview-buttons-format.scss";
+import { useState } from "react";
 import Desktop from "../../../../public/images/pictos/desktop.svg";
 import Tablet from "../../../../public/images/pictos/tablet.svg";
 import Mobile from "../../../../public/images/pictos/mobile.svg";
+import classNames from "classnames";
 import Image from "next/image";
+import "./preview-buttons-format.scss";
 
 interface IEditoPreviewProps {
   onFormatting: (device: string) => void;
@@ -10,29 +12,66 @@ interface IEditoPreviewProps {
 export default function PreviewButtonsFormat({
   onFormatting,
 }: IEditoPreviewProps) {
+  type DeviceType = "desktop" | "tablet" | "mobile";
+
+  const [isActive, setActive] = useState("desktop");
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const device = e.currentTarget.id as DeviceType;
+    setActive(device);
+    onFormatting(device);
+  };
+
   return (
     <div className="c-PreviewButtonsFormat">
       <button
-        className="c-PreviewButtonsFormat__Button"
+        className={classNames("c-PreviewButtonsFormat__Button", {
+          "c-PreviewButtonsFormat__Button_active": isActive === "desktop",
+        })}
+        id="desktop"
         type="button"
-        // TODO: handle the desktop button
-        // onClick={handleDeleteClick}
+        onClick={(e) => handleClick(e)}
       >
-        <Image src={Desktop} alt="" width={25} height={25} />
+        <Image
+          className="c-PreviewButtonsFormat__Button_icon"
+          src={Desktop}
+          alt=""
+          width={25}
+          height={25}
+        />
       </button>
       <button
-        className="c-PreviewButtonsFormat__Button"
+        className={classNames("c-PreviewButtonsFormat__Button", {
+          "c-PreviewButtonsFormat__Button_active": isActive === "tablet",
+        })}
+        id="tablet"
         type="button"
-        onClick={() => onFormatting("tablet")}
+        onClick={(e) => handleClick(e)}
       >
-        <Image className="test" src={Tablet} alt="" width={25} height={25} />
+        <Image
+          className="c-PreviewButtonsFormat__Button_icon"
+          src={Tablet}
+          alt=""
+          width={25}
+          height={25}
+        />
       </button>
       <button
-        className="c-PreviewButtonsFormat__Button"
+        className={classNames("c-PreviewButtonsFormat__Button", {
+          "c-PreviewButtonsFormat__Button_active": isActive === "mobile",
+        })}
+        id="mobile"
         type="button"
-        onClick={() => onFormatting("mobile")}
+        onClick={(e) => handleClick(e)}
       >
-        <Image src={Mobile} alt="" width={25} height={25} />
+        <Image
+          className="c-PreviewButtonsFormat__Button_icon"
+          src={Mobile}
+          alt=""
+          width={25}
+          height={25}
+        />
       </button>
     </div>
   );
